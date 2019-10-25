1a8d3fa034ef22e543726c8cdcb7691c9f59ac21
Krzysiek <kgrygiel@gmail.com>
2019-10-25 21:59:08 +0200

    store

--- server.js
+++ server.js
@@ -2,6 +2,14 @@ var express = require('express');
 var app = express();
 app.use(express.static('assets'));
 
+app.use('/store', function(req, res, next){
+    console.log('Jestem pośrednikiem przy żądaniu do /store');
+    next();
+});
+
+app.get('/store', function (req, res) {
+    res.send('To jest sklep');
+});
 
 app.get('/', function (req, res) {
     res.sendFile('/index.html');
